371a841cf0862ff9efb25c60d865676b543498b6
minsunnyya <sun2547@naver.com>
2025-02-05 14:58:08 +0900

    Feature: 비밀번호 생성기

---
+++ password/script.js
@@ -0,0 +1,9 @@
+document.getElementById("generate").addEventListener("click", function () {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  let password = "";
+  for (let i = 0; i < 12; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  document.getElementById("password").value = password;
+});
